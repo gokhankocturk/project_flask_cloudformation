f9d32361046acf05b4d11159f96ec3a0c6cd1152
gokhankocturk <gokhankocturk7@gmail.com>
2023-12-15 00:20:28 -0800

    yaml updated

--- app.py
+++ app.py
@@ -26,23 +26,5 @@ def main_post():
         return render_template('index.html', developer_name='Gokhan', not_valid=False)
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    # app.run(host='0.0.0.0', port=80)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-if __name__ == '__main__':
-    # app.run(debug=True)
-    app.run(host='0.0.0.0', port=8080)
+    # app.run(debug=True)
+    app.run(host='0.0.0.0', port=8081)
